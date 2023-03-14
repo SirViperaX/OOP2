182109c51fae1aa326c749178eac2e63d5205201
Serban Balog <iustinel78@gmail.com>
2023-03-14 09:13:48 +0200

    Add project files.

---
+++ OOP2/MyDateTime.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace OOP2
+{
+    internal class MyDateTime
+    {
+        private int days;
+        private int months;
+        private int years;
+        private int hours;
+        private int minutes;
+        private int seconds;
+
+        public MyDateTime(int days = 0, int months = 0, int years = 0, int hours = 0, int minutes = 0, int seconds = 0)
+        {
+            this.days = days;
+            this.months = months;
+            this.years = years;
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(this.days)
+              .Append(' ')
+              .Append(string.Format("{0,2:D2}",this.months))
+              .Append(' ')
+              .Append(this.years)
+              .Append(' ')
+              .Append(this.hours)
+              .Append(':')
+              .Append(this.minutes)
+              .Append(':')
+              .Append(this.seconds);
+            return sb.ToString();
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            MyDateTime md = new MyDateTime(14, 3, 2023, 0, 0, 0);
+            Console.WriteLine(md);
+            MyDateTime md2 = new MyDateTime(23, 6, 2023);
+        }
+    }
+}
